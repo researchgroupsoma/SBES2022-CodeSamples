6f4280d3804a684c4db9af6afb50fdc4e8716957
Gabriel Menezes <gabrielufmscc@gmail.com>
2020-06-25 09:04:59 -0400

    Adicionando prints de status ao extrair as perguntas do stackorverflow

--- stackoverflow/stackoverflow.py
+++ stackoverflow/stackoverflow.py
@@ -1,6 +1,6 @@
 from stackapi import StackAPI
 from datetime import datetime
-from utils import output_write, get_samples
+from utils import output_write, get_samples, print_status_samples
 
 # max_date = datetime(year=2019, month=4, day=20)
 directory = "stackoverflow"
@@ -10,7 +10,7 @@ def get_questions_when_body_has(sample):
     return api.fetch("search/advanced?body={0}".format(sample))
 
 
-def get_top_five_tags(user_id):
+def get_top_five_tags(api, user_id):
     tags = []
     page = 1
     query = api.fetch("users/{0}/top-tags".format(user_id), page=page)
@@ -37,8 +37,8 @@ def list_to_string(list):
 
 def create_output(framework, sample, question, answer, question_owner, answer_owner):
     question_tags = list_to_string(question["tags"])
-    question_owner_tags = list_to_string(get_top_five_tags(question_owner["user_id"]))
-    answer_owner_tags = [] if answer_owner["user_id"] == "" else list_to_string(get_top_five_tags(answer_owner["user_id"]))
+    question_owner_tags = list_to_string(get_top_five_tags(api, question_owner["user_id"]))
+    answer_owner_tags = [] if answer_owner["user_id"] == "" else list_to_string(get_top_five_tags(api, answer_owner["user_id"]))
     output = framework + "," + sample
     output = output + "," + str(question["question_id"]) + "," + question_tags + "," + str(question["view_count"]) + "," + str(question["answer_count"]) + "," + str(question["score"]) + "," + str(datetime.fromtimestamp(question["creation_date"])) + "," + str(question["link"]) + "," + str(question_owner["user_id"]) + "," + str(question_owner["reputation"]) + "," + str(datetime.fromtimestamp(question_owner["creation_date"])) + "," + str(question_owner_tags)
     output = output + "," + str(answer["answer_id"]) + "," + str(answer["score"]) + "," + ("" if answer["creation_date"] == "" else str(datetime.fromtimestamp(answer["creation_date"]))) + "," + str(answer_owner["user_id"]) + "," + str(answer_owner["reputation"]) + "," + ("" if answer_owner["creation_date"] == "" else str(datetime.fromtimestamp(answer_owner["creation_date"]))) + "," + str(answer_owner_tags)
@@ -58,7 +58,7 @@ def get_header():
            "answer_owner_tags"
 
 
-def get_owner_by_user_id(id):
+def get_owner_by_user_id(api, id):
     return api.fetch("users", ids=[id])["items"][0]
 
 
@@ -67,12 +67,14 @@ def stackoverflow(framework, projects):
     api = StackAPI("stackoverflow")
     samples = get_samples(projects)
     output_write(framework, directory, "questions_and_answers", get_header(), True)
-    for sample in samples:
+    for index, sample in enumerate(samples):
+        print_status_samples(index+1, len(samples))
         questions = get_questions_when_body_has(sample)
-        for question in questions["items"]:
+        for indx, question in enumerate(questions["items"]):
+            print("{0}% questions analysed of {1}".format( (indx+1)/len(questions)*100, sample))
             try:
                 answer = api.fetch("answers/{ids}", ids=[question["accepted_answer_id"]])["items"][0]
-                answer_owner = get_owner_by_user_id(answer["owner"]["user_id"])
+                answer_owner = get_owner_by_user_id(api, answer["owner"]["user_id"])
             except KeyError:
                 answer = {
                     "answer_id": "",
@@ -85,6 +87,6 @@ def stackoverflow(framework, projects):
                     "creation_date": "",
                     "tags": []
                 }
-            question_owner = get_owner_by_user_id(question["owner"]["user_id"])
+            question_owner = get_owner_by_user_id(api, question["owner"]["user_id"])
             output = create_output(framework, sample, question, answer, question_owner, answer_owner)
             output_write(framework, directory, "questions_and_answers", output, False)
